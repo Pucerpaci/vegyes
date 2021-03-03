26b676ce24139cb7dff38920aed33839e8b1af15
Pucerpaci <78861795+Pucerpaci@users.noreply.github.com>
2021-03-03 14:38:43 +0100

    Update script.js

--- script.js
+++ script.js
@@ -391,33 +391,3 @@ document.querySelector("#btn-újra").addEventListener("click", function () {
   document.querySelector("#válasz3").disabled = false;
   document.querySelector("#válasz4").disabled = false;
 });
-
-// MODAL WINDOW:
-
-const modal = document.querySelector(".modal");
-const overlay = document.querySelector(".overlay");
-const btnCloseModal = document.querySelector(".close-modal");
-
-const btnsOpenModal = document.querySelectorAll(".show-modal");
-
-const openModal = function () {
-  modal.classList.remove("hidden");
-  overlay.classList.remove("hidden");
-};
-
-const closeModal = function () {
-  modal.classList.add("hidden");
-  overlay.classList.add("hidden");
-};
-
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener("click", openModal);
-
-btnCloseModal.addEventListener("click", closeModal);
-overlay.addEventListener("click", closeModal);
-
-document.addEventListener("keydown", function (e) {
-  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
-    closeModal();
-  }
-});
